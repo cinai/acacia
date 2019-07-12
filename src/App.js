9844740dbd04dedffbad00690094199ad5f9300c
cinai <ceinaipil@gmail.com>
2019-07-12 13:40:28 -0400

    celular diferente

--- src/App.js
+++ src/App.js
@@ -6,18 +6,42 @@ import Quienes from './components/Quienes';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          Acacia
+    let width = window.innerWidth;
+    if (width > 708) {
+      return (
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            Acacia
+          </div>
+          <Portada/>
+          <div className="Bar"></div>
+          <Quienes/>
         </div>
-        <Portada/>
-        <div className="Bar"></div>
-        <Quienes/>
-      </div>
-    );
+      );
+    }
+    else {
+      return (
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            Acacia
+          </div>
+        <header className="viewport-header">
+          <span className="text-header5">
+            <h3>Machine Learning para<br/> Business Intelligence </h3>
+          </span>
+        </header>
+        <header className="viewport-header3">
+          <span className="text-header6">
+            AcacIA ofrece soluciones de softwares basados en Inteligencia Artificial 
+            que permiten la automatización y optimización de distintos procesos operacionales de su compañía, apoyando y mejorando la toma de decisiones.
+          </span>
+        </header>
+        </div>
+      );
+    }
   }
-}
+} 
 
 export default App;
