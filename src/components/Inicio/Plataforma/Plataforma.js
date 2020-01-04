f24b0c422e250b84aecb2492b22100b10d220d41
cinai <ceinaipil@gmail.com>
2020-01-04 10:50:45 -0300

    agregar archivos

--- src/components/Inicio/Plataforma/Plataforma.js
+++ src/components/Inicio/Plataforma/Plataforma.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Plataforma.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChartLine } from '@fortawesome/free-solid-svg-icons';
+import { faChartLine, faSignOutAlt, faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
@@ -9,6 +9,14 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import IconButton from '@material-ui/core/IconButton';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import Tooltip from '@material-ui/core/Tooltip';
 
 
 function TabPanel(props) {
@@ -58,17 +66,79 @@ const Plataforma = props => {
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+
+  const handleToggle = () => {
+    setOpen(prevOpen => !prevOpen);
+  };
+
+  const handleClose = event => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event) {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      setOpen(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
   return (
   <div className="plataforma">
     <div className="plataforma-header">
-     
       <div className={classes.root}>
         <AppBar position="static" color="transparent">
           <div className="plataforma-proyecto">
-            <span className="box">
-              <FontAwesomeIcon icon={faChartLine} size="lg"></FontAwesomeIcon>
-            </span>
-            <h3>Predicción de demanda de insumos y medicamentos</h3>
+            <div className="plataforma-titulo">
+              <span className="box">
+                <FontAwesomeIcon icon={faChartLine} size="lg"></FontAwesomeIcon>
+              </span>
+              <h3>Predicción de demanda de insumos y medicamentos</h3>
+            </div>
+            <div className="plataforma-user">
+              <Tooltip title="Catalina Espinoza">
+                <IconButton 
+                    ref={anchorRef}
+                    aria-controls={open ? 'menu-list-grow' : undefined}
+                    aria-haspopup="true"
+                    onClick={handleToggle}
+                    size="small"
+                >
+                  <FontAwesomeIcon icon={faUserCircle} size="lg" className="plataforma-user-icon" />
+                </IconButton >
+              </Tooltip>
+              <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+                {({ TransitionProps, placement }) => (
+                  <Grow
+                    {...TransitionProps}
+                    style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+                  >
+                    <Paper>
+                      <ClickAwayListener onClickAway={handleClose}>
+                        <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+                          <MenuItem onClick={handleClose}> <FontAwesomeIcon icon={faSignOutAlt} size="s" />Cerrar sesión</MenuItem>
+                        </MenuList>
+                      </ClickAwayListener>
+                    </Paper>
+                  </Grow>
+                )}
+              </Popper>
+            </div> 
           </div>
           <Tabs 
             value={value} 
